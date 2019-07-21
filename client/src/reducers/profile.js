5fc81af7390be340c520b1f51ba35f4c6916ae5d
Jananie-wathsala <jana.kum@hotmail.com>
2019-07-21 11:43:06 +0530

    finish profile actions and reducers

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -2,7 +2,8 @@ import {
   GET_PROFILE,
   PROFILE_ERROR,
   CLEAR_PROFILE,
-  UPDATE_PROFILE
+  UPDATE_PROFILE,
+  GET_REPOS
 } from "../actions/types";
 
 const initialState = {
@@ -22,6 +23,12 @@ export default function(state = initialState, action) {
         profile: payload,
         loading: false
       };
+    case GET_PROFILEs:
+      return {
+        ...state,
+        profiles: payload,
+        loading: false
+      };
     case PROFILE_ERROR:
       return {
         ...state,
@@ -33,6 +40,12 @@ export default function(state = initialState, action) {
         ...state,
         profile: null,
         repos: [],
+        loading: false
+      };
+    case GET_REPOS:
+      return {
+        ...state,
+        repos: payload,
         loading: false
       };
     default:
