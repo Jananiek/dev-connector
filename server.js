642801fc28620d6b07695ba255e2b95170486fbf
Jananie-wathsala <jana.kum@hotmail.com>
2019-07-22 19:00:50 +0530

    fixed error on heroku

--- server.js
+++ server.js
@@ -19,5 +19,5 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
-const PORT = process.env.PORT | 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on ${PORT}`));
